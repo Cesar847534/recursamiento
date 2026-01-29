0d66d72d4d48f971cedc9b38772f78280e3b37d9
cesar <merc090212hgtnmsa3@cbtis217.edu.mx>
2026-01-29 13:33:56 -0600

    primer programa c++ EncuentraOcho

---
+++ EncuebtraOcho.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+
+    // Pedir un número al usuario
+    cout << "Ingresa un numero: ";
+    cin >> numero;
+
+    // Verificar si el numero es 8
+    if (numero == 8) {
+        cout << "Encontraste el ocho!" << endl;
+    } else {
+        cout << "El numero no es ocho." << endl;
+    }
+
+    return 0;
+}
